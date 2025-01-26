8cfc670ce6419141a3a7a6f418e3ffd892ae733a
Joris Kampman (prive) <joriskampman@gmail.com>
2025-01-26 16:45:17 +0100

    whooops, forgot to save some files

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# pylint: disable=C0114,C0116
-=======
->>>>>>> 8dfc5c0d7a9da5742bd655b1c45715c0cc800bc5
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -14,20 +10,6 @@ def generate_launch_description():
                             'config',
                             'joystick.yaml')
 
-<<<<<<< HEAD
-  joy_node = Node(package='joy',
-                  executable='joy_node',
-                  parameters=[joy_params])
-
-  teleop_node = Node(package='teleop_twist_joy',
-                  executable='teleop_node',
-                  name='teleop_node',
-                  parameters=[joy_params],
-                  remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')])
-
-  return LaunchDescription([
-    joy_node,
-=======
   # joy_node = Node(package='joy',
   #                 executable='joy_node',
   #                 parameters=[joy_params])
@@ -40,6 +22,5 @@ def generate_launch_description():
 
   return LaunchDescription([
     # joy_node,
->>>>>>> 8dfc5c0d7a9da5742bd655b1c45715c0cc800bc5
     teleop_node,
   ])
